91b42f822889213cef5237ea9f4d25e1b4d2a392
ahmdirfnz <irfanzorro6985@gmail.com>
2023-07-04 21:18:48 +0800

    make todo list

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task Management App',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Task Management App'),
     );
   }
 }
@@ -32,10 +32,72 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final List<Todo> _todos = <Todo>[];
+  final TextEditingController _textEditingController = TextEditingController();
+
+  Future<void> _displayDialog() async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a todo'),
+            content: TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(hintText: 'Add your task'),
+              autofocus: true,
+            ),
+            actions: [
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                  onPressed: () {
+                  Navigator.of(context).pop();
+                  }, 
+                  child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  )
+                ),
+                  onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textEditingController.text);
+                  }, 
+                  child: const Text('Add')
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  void _addTodoItem(String name) {
+    setState(() {
+      _todos.add(Todo(name: name, completed: false));
+      _textEditingController.clear();
+    });
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.completed = !todo.completed;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _deleteTodo(Todo todo) {
+    setState(() {
+      _todos.removeWhere((element) => element.name == todo.name);
     });
   }
 
@@ -47,27 +109,99 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: _todos.map((Todo todo) {
+          return TodoItem(
+            todo: todo,
+            onTodoChanged: _handleTodoChange,
+            removeTodo: _deleteTodo,
+          );
+        }).toList(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _displayDialog,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+class TodoItem extends StatelessWidget {
+  var removeTodo;
+
+  TodoItem({required this.todo, required this.onTodoChanged, required this.removeTodo}) : super(key: ObjectKey(todo));
+
+  final Todo todo;
+
+  final void Function(Todo todo) onTodoChanged;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onTodoChanged(todo);
+      },
+      leading: Checkbox(
+        checkColor: Colors.white,
+          activeColor: Colors.green,
+          value: todo.completed,
+          onChanged: (value) {
+          onTodoChanged(todo);
+          },
+      ),
+      title: Row(
+        children: [
+          Expanded(
+              child: Text(todo.name, style: _getTextStyle(todo.completed)),
+          ),
+          IconButton(
+              onPressed: (){
+                removeTodo(todo);
+              },
+              iconSize: 30,
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            alignment: Alignment.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all()
+      ),
+    );
+  }
+}
+
+class Todo {
+  Todo({required this.name, required this.completed});
+  String name;
+  bool completed;
+}
+
